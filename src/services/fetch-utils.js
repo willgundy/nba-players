5998f726fb84678fcc5317f196475467d57427c9
Will Gundy <willgunderson503@gmail.com>
2022-05-10 16:44:12 -0700

    wrote fetch utils without joins

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,22 @@
+import { client } from './client';
+
+
+export async function getNBAPlayers(from = 0, to = 20, perPage = 20) {
+  const response = await client 
+    .from('nba-players')
+    .select('*', { count: 'exact' })
+    .range(to, from);
+
+  const lastPage = Math.ceil(response.count / perPage);
+    
+  return { ...response, lastPage };
+}
+
+export async function getSinglePlayer(id) {
+  const response = await client 
+    .from('nba-players')
+    .select('*')
+    .match({ personId: id });
+    
+  return { response };
+}
